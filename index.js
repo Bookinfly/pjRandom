19388bbe49769cda09aa196c68371ac23f130960
Nahuel Amado <amado.nahuel@outlook.com>
2024-09-13 18:52:54 -0300

    equipo y protección agregado, estuve trabajando en los html creados y el css, logre una separación mejor para ver todos los datos, pero los problemas que genere son demasiados

--- index.js
+++ index.js
@@ -101,18 +101,28 @@ const habilidadesRandom = (raza, profesion) => {
 }
 
 ///Le da cierto grado de azhar a la tenencia de armas medievales
-function armas(){
+function equipo(){
     let armaEscondida = loTrae(6);
     let armaCorta = loTrae(10);
     let armaLarga = loTrae(3);
     let armaDistacia = loTrae(3);
     let armaDeCalidad = loTrae(2);
     let municion = parseInt(Math.random() * 30);
-    return [armaEscondida, armaCorta, armaLarga, armaDistacia, armaDeCalidad, municion];
+    let armadura = loTrae(3);
+    let casco = loTrae(3);
+    let escudo = loTrae(3);
+    let tesoro = parseInt(Math.random() * 5) +1;
+    let mochila = loTrae(6);
+    let muda = loTrae(3);
+    let capa = loTrae(4);
+    let provisiones = parseInt(Math.random() * 30);
+    let cuerda = loTrae(3);
+    let equipoProf = loTrae(3);
+    return [armaEscondida, armaCorta, armaLarga, armaDistacia, armaDeCalidad, municion, armadura, casco, escudo, tesoro, mochila, muda, capa, provisiones, cuerda, equipoProf];
 };
 
 function equipadas(profesionMedieval){
-    let [armaEscondida, armaCorta, armaLarga, armaDistacia, armaDeCalidad, municion] = armas();//inicia desestructurando armas()
+    let [armaEscondida, armaCorta, armaLarga, armaDistacia, armaDeCalidad, municion, armadura, casco, escudo, tesoro, mochila, muda, capa, provisiones, cuerda, equipoProf] = equipo();//inicia desestructurando equipo()
     //console.log(profesionMedieval + " " + armaEscondida + " " +  armaCorta + " " +  armaLarga + " " +  armaDistacia + " " +  armaDeCalidad + " " +  municion)
     ////////casos estandar
     let escondida = armaEscondida == true ? "Cuchillo: Combate +0 ":"";
@@ -121,6 +131,15 @@ function equipadas(profesionMedieval){
     let distancia = armaDistacia == true ? "Arco: Combate +1 ":"";
     let calidad = armaDeCalidad == true ? "Espada Larga: Combate +2 Iniciativa +1 " : "";
     municion = armaDistacia == true ? municion : "";
+    escudo = escudo == true ? "Escudo +1 Defensa": "";
+    casco = casco == true ? "casco +1 Defensa": "";
+    armadura = armadura == true ? "Armadura ligera +1 Defensa": "";
+    tesoro = `${tesoro * 3} monedas de cobre + ${tesoro } monedas de plata`;
+    mochila = mochila == true ? "Mochila de viaje, con equipo de acampar, yesquero, una antorcha, cuenco de cocina y pieles":"Tiene una bolsa colgada al hombro, podrías guardar algo ahí";
+    muda = muda == true ? "Un cambio de ropa":"";
+    capa = capa == true ? "Capa de viajero con capucha":"";
+    provisiones = `Provisiones de viaje para ${provisiones} días`
+    cuerda = cuerda == true ? "Una cuerda, ninguna aventura debería comenzar sín una":"";    
     //console.log(profesionMedieval + " " + escondida  + " " + corta +  + " " + larga + " " + distancia  + " " + calidad  + " " +  municion);
 
     ////////////excepciones
@@ -129,25 +148,39 @@ function equipadas(profesionMedieval){
         escondida = "Daga: Combate +0";
         distancia = armaDistacia == true ? "Ballesta: Combate + 2":"";
         larga = armaLarga == true ? "Espada a 2 manos: Combate + 2":"";
+        armadura = armadura == true ? "Armadura Pesada +3 Defensa": "Armadura de Cota +2 Defensa";
+        equipoProf = equipoProf == true ? "Piedra de afilar, equipo de sutura, sello familiar, papiro, pluma y tinta":"";
     } else if ( profesionMedieval ===  "Clériga Guerrera" ||profesionMedieval ===  "Mercenario"|| profesionMedieval ===  "Barbaro" || profesionMedieval === "Mercader Enano") {
         corta = armaCorta == "Masa de Armas: Combate +1";
         distancia = armaDistacia == true ? "Ballesta: Combate + 2": "Resortera Combate -1";
         calidad = "Hacha de Batalla: Combate +2 Iniciativa +1 ";
+        armadura = armadura == true ? "Armadura de Cota +2 Defensa": "Armadura ligera +1 Defensa";
     } else if (profesionMedieval ===  "Mago"){
         larga = "Cayado: Combate +1, + un hechizo de luz gratis";
         distancia = "Anillo hechizado + 1 hechizo de ataque +0";
-        municion = "El anillo guarda 1 de magia para su hechizo, se puede recargar"
+        municion = "El anillo guarda 1 de magia para su hechizo, se puede recargar";
+        escudo = "";
+        casco = "";
+        armadura = armadura == true ? "Amuleto protector, te salva una vez, requiere 9 de magia para recargar": "Frasco de Neblina Fatua +1D8 defensa, un solo uso";
+        equipoProf = equipoProf == true ? "Bolsitas con hiervas, unguentos, frascos con elixires y toda clase de polvos":"";
     } else if (profesionMedieval === "Arquera" || profesionMedieval === "Pícaro" || profesionMedieval === "Aventurera" ){
         larga = "";
         distancia = armaDistacia == true ? "Arco de caza: Combate + 2 + 1 Iniciativa":"Arco: Combate + 1";
         escondida = "Daga: Combate +0";
         calidad = "Daga Curva: Combate +1 Iniciativa +1";
+        equipoProf = equipoProf == true ? "Ganzúas, cuerda y gancho":"";
     } else if (profesionMedieval === "Explorador" || profesionMedieval === "Cazador Goblin" || profesionMedieval === "Guardia Halfling") {
         larga = armaLarga == true ? "Lanza: Combate + 2": "Baston de viajero ataque +0";
         distancia = armaDistacia == true ? "Arco Corto: Combate +1":"Arco: Combate +0";
         corta = armaCorta == true ? "Espada Corta: Combate + 1":"Garrote + Combate +0";
+        armadura = armadura == true ? "Armadura ligera de calidad +2 Defensa": "Armadura ligera +1 Defensa";
+    } else {
+        equipoProf = equipoProf == true ? "Algún elemento de tu profesión combeniente para la trama":"";
     }
-    return[escondida, corta, larga, distancia, calidad, municion];
+    if (profesionMedieval === "Caballero" || profesionMedieval === "Paladin" || profesionMedieval === "Guerrero" || profesionMedieval === "Noble" || profesionMedieval === "Esgrimista" || profesionMedieval === "Mago" || profesionMedieval === "Mercader Enano" || profesionMedieval === "Aventurera" ){
+        tesoro = tesoro + ", 3 monedas de oro";
+    }
+    return[escondida, corta, larga, distancia, calidad, municion, escudo, casco, armadura, tesoro, mochila, muda, capa, provisiones, cuerda, equipoProf];
 }       
 
 
@@ -176,41 +209,59 @@ function crearAventurero() {
     hMagia += habilidadesAleatorias[3];
     hSociales += habilidadesAleatorias[4];
     //////////EQUIPO
-    [escondida, corta, larga, distancia, calidad, municion] = equipadas(profesionMedieval);
+    [escondida, corta, larga, distancia, calidad, municion, escudo, casco, armadura, tesoro, mochila, muda, capa, provisiones, cuerda, equipoProf] = equipadas(profesionMedieval);
     /////////HTML
     let idPj = "Pj" + (contador - 1); // Asigna un ID al personaje
     let img = ilustraciones[contador - 1]; // Selecciona la ilustración correspondiente
     // Genera el HTML de las profesiones, características y habilidades del aventurero
-    let profesionHeroeHTML = `<h3>Profesión ${profesion[contador - 1][0]}</h3>`;
-    let nombreHTML = `<h3 id='${idPj}'>${nombreHeroe}</h3>`;
-    let razaHTML = `<p>Raza: <b>${razaHeroe}</b></p>`;
+    let profesionHeroeHTML = `<div class="separador><h3 class="separador>Profesión ${profesion[contador - 1][0]}</h3><br></div>`;
+    let nombreHTML = `<div class="separador><h3 class="separador id='${idPj}'>${nombreHeroe}</h3><br></div>`;
+    let razaHTML = `<div class="separador><p>Raza: <b>${razaHeroe}</b></p></div>`;
     let caracteristicasHTML = 
-    `<p>
+    `<div class="separador"><p>
         <b>Características</b><br>
         Fuerza ${fuerza}<br>
         Agilidad ${agilidad}<br>
         Inteligencia ${inteligencia}<br>
         Carisma ${carisma}<br>
-        <b class="pv">Puntos de Vida ${fuerza*3}</b>
-    </p>`;
-    let habilidadesHTML = `<p>
+        <b class="pv">Puntos de Vida ${fuerza*3}</b></p>
+    </div>`;
+    let habilidadesHTML = `<div class="oculto separador">
+        <p>
         <b>Habilidades</b><br>
         Combate ${hCombate}<br>
         Conocimiento ${hConocimiento}<br>
         Latrocinio ${hLatrocinio}<br>
         Magia ${hMagia}<br>
-        Sociales ${hSociales}<br>
-    </p>`;
-    let armasHTML = `<p class="armas">
-        <b>Armamento Inicial</b><br>
+        Sociales ${hSociales}<br></p>
+    </div>`;
+    let armasHTML = `<div class="armas oculto separador">
+        <p><b>Armamento</b><br>
         Arma Oculta: ${escondida}<br>
         Arma Corta: ${corta}<br>
         Arma Larga: ${larga}<br>
         Arma de Distancia: ${distancia}<br>
         Munición: ${municion}<br>
-        Arma Especial: ${calidad}<br>   
-    </p>`;
-    return [img, profesionHeroeHTML, razaHTML, nombreHTML, caracteristicasHTML, habilidadesHTML, armasHTML];
+        Arma Especial: ${calidad}<br></p>  
+    </div>`;
+    let armaduraHTML = `<div class="armas oculto separador">
+        <p><b>Protección</b><br>
+        Casco: ${casco}<br>
+        Armadura: ${armadura}<br>
+        Escudo: ${escudo}<br></p>
+    </div>`;
+    let equipoHTML = `<div class="armas oculto separador">
+        <p><b>Equipo</b><br>
+        Tesoro: ${tesoro}<br>
+        Mochila: ${mochila}<br>
+        Muda: ${muda} <br>
+        Capa: ${capa}<br>
+        Provisiones: ${provisiones}<br>
+        Cuerda: ${cuerda}<br>
+        EquipoProf: ${equipoProf}<br>
+        Liquido: Llevas on odre de agua<br></p>
+    </div>`;
+    return [img, profesionHeroeHTML, razaHTML, nombreHTML, caracteristicasHTML, habilidadesHTML, armasHTML, armaduraHTML, equipoHTML];
 }
 
 // Función que cambia el valor oculto (hidden) del campo del formulario
@@ -223,12 +274,13 @@ let documentFragment = document.createDocumentFragment(); // Crea un fragmento d
 // Itera para generar 20 aventureros
 for (var i = 1; i <= 20; i++) {
     //let precioRandom = Math.round(Math.random()*10+30);
-    [img,profesionHeroeHTML,razaHTML,nombreHTML,caracteristicasHTML, habilidadesHTML,armasHTML] = crearAventurero();//invova función crear llave, inserta datos con template literals, la iteración se vuelve el nombre
+    [img,profesionHeroeHTML,razaHTML,nombreHTML,caracteristicasHTML, habilidadesHTML,armasHTML,armaduraHTML, equipoHTML] = crearAventurero();//invova función crear llave, inserta datos con template literals, la iteración se vuelve el nombre
     let div = document.createElement("div");//creamos un contenedor en memoria
+    div.classList.add('pj');//clase para dejar de ocultar elementos
     div.setAttribute("tabindex", "0"); // Esto permite que el div reciba el foco
     div.addEventListener("click", ()=>changeHidden());
     div.classList.add(`flex-item`,`item-${i}`);//le sumamos clases como atributos
-    div.innerHTML = img + profesionHeroeHTML + razaHTML + nombreHTML + caracteristicasHTML + habilidadesHTML + armasHTML;//inserta elementos del array llave en el div antes creado.
+    div.innerHTML = img + profesionHeroeHTML + razaHTML + nombreHTML + caracteristicasHTML + habilidadesHTML + armasHTML + armaduraHTML + equipoHTML;//inserta elementos del array llave en el div antes creado.
     // contenedor.innerHTML += div;
     documentFragment.appendChild(div);//agregamos el div como hijo del documentFragment
 }
