dccf211c5ff494a02f7fe19861d427b8dfab4085
Nahuel Amado <amado.nahuel@outlook.com>
2024-09-24 17:35:00 -0300

    Solucionados los problemas con las rutas de img y probando solucion con las rutas al css y js de html generado con el boton

--- index.js
+++ index.js
@@ -328,13 +328,13 @@ document.getElementById("descargarPDF").addEventListener("click", ()=>{
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/stylePj.css">
+    <link rel="stylesheet" href="https://bookinfly.github.io/pj_random/stylePJ.css">
 </head>
 <body>
 <main >
         <div class="flex-container">${cardElement}</div>
         <button>Imprimir</button>
-        <script src="/boton.js"></script>
+        <script src="https://bookinfly.github.io/pj_random/boton.js"></script>
 </main>
 </body>
 </html>`);
